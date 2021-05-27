6f5629fd21ca39c1d13caca5fc1b2299071fb977
Guilherme <santosguilhermes2001@gmail.com>
2021-05-27 12:53:39 -0300

    Finalizando a primeira etapa

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
+  activeTab: any;
   constructor() {}
-
+  
+  tabChange(event){
+    this.activeTab = event.tab;
+  }
 }
